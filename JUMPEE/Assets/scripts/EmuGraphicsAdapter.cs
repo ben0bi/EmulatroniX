75b6ba8a7cfa89160c12623903f81a706969db4e
DEEP THOUGHT v2 [EARTH] <DEEP@THOUGHTS.question>
2019-10-14 08:49:55 +0200

    Graphics setup seems to work.

--- JUMPEE/Assets/scripts/EmuGraphicsAdapter.cs
+++ JUMPEE/Assets/scripts/EmuGraphicsAdapter.cs
@@ -8,33 +8,62 @@ public class EmuGraphicsAdapter : MonoBehaviour
     protected Vector2 sprite_size; //= GetComponent<SpriteRenderer>().sprite.rect.size;
     protected Vector2 local_sprite_size; //= sprite_size / GetComponent<SpriteRenderer>().sprite.pixelsPerUnit;
 
-    // we need two images for the buffering process: one to draw on and one to show meanwhile.
-    public GameObject Display_Image_1;
-    public GameObject Display_Image_2;
+    // The actual emulator display game object which is visible on the screen.
+    protected GameObject m_display_image;
+    protected SpriteRenderer m_display_renderer;
+
+    // at least 2 sprites for the buffering process.
+    public int Buffer_Size = 2;
+    protected Sprite[] m_buffer_sprites;
+    protected int m_actualBufferIndex = 0;
 
     // keep the emulator aspect ratio?
     public bool keepAspectRatio = false;
+#if (UNITY_EDITOR)
+    private bool m_oldKeepAspectRatio = false;
+#endif
 
-    protected GameObject m_actual_display_image;    // this is actual the image to show.
-    protected GameObject m_actual_draw_image;       // this is actual the image to draw on.
     protected Texture2D m_drawable_texture;         // the actual texture to draw on.
     protected Sprite m_drawable_sprite;             // the sprite with the texture to draw on.
-    Color Reset_Colour = Color.blue;
-    Color32[] m_cur_colors;                           // array with the pixel colors from the texture to draw on.
-    Color[] m_clean_colours_array;
+    Color32 Reset_Colour = new Color32(0,0,69,0xFF);              // color for resetting the display.
+    Color32[] m_cur_colors;                         // array with the pixel colors from the texture to draw on.
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        m_oldKeepAspectRatio = keepAspectRatio;
+
+        if (Buffer_Size < 1)
+            Buffer_Size = 1;
+
+        Debug.Log("EmuGraphicsAdapter started.");
+
+        // create the display image game object.
+        m_display_image = new GameObject("DISPLAY");
+        m_display_image.AddComponent<SpriteRenderer>();
+        // get the renderer to not search for it in each frame.
+        m_display_renderer = m_display_image.GetComponent<SpriteRenderer>();
+
+        // set some settings on the renderer.
+        m_display_renderer.flipY = true;
+        m_display_renderer.receiveShadows = false;
+        m_display_renderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
+
+        setEmuScreenSize(50, 50);
+    }
 
     // call this function after the screen has resized or the sprite has changed.
     protected void resized()
     {
         // resize.
-        var sprr = Display_Image_1.GetComponent<SpriteRenderer>();
+        var sprr = m_display_image.GetComponent<SpriteRenderer>();
 
-        // we don't need that but it's the real image size in pixels for later.
+        // The real image size in pixels for later.
         sprite_size = sprr.sprite.rect.size;
+        // and in local space units.
         local_sprite_size = sprite_size / sprr.sprite.pixelsPerUnit;
 
         // get the screen height & width in world space units
-        // get the sprite width in world space units
         float worldSpriteWidth = sprr.sprite.bounds.size.x;
         float worldSpriteHeight = sprr.sprite.bounds.size.y;
 
@@ -42,10 +71,7 @@ public class EmuGraphicsAdapter : MonoBehaviour
         float worldScreenWidth = (worldScreenHeight / Screen.height) * Screen.width;
 
         // initialize new scale to the current scale
-        Vector3 newScale = Display_Image_1.transform.localScale;
-
-        // get the aspect ratio of the sprite image.
-        float aspectRatio = worldSpriteWidth / worldSpriteHeight;
+        Vector3 newScale = m_display_image.transform.localScale;
 
         // resize the local scale to the world scale.
         newScale.x = worldScreenWidth / worldSpriteWidth;
@@ -54,29 +80,23 @@ public class EmuGraphicsAdapter : MonoBehaviour
         // maybe keep the aspect ratio
         if (keepAspectRatio)
         {
-            float newx = newScale.x;
-            float newy = newScale.x * aspectRatio;
-            float wrldy = newy * local_sprite_size.y;
-            // new height is bigger than screen height, switch aspect ratio calculation.
-            if (wrldy > worldScreenHeight)
-            {
+           // get the aspect ratio of the sprite image.
+           float aspectRatio = worldSpriteWidth / worldSpriteHeight;
+           float newx = newScale.x;
+           float newy = newScale.x * aspectRatio;
+           float wrldy = newy * local_sprite_size.y;
+           // new height is bigger than screen height, switch aspect ratio calculation.
+           if (wrldy > worldScreenHeight)
+           {
                 newy = newScale.y;
-                newx = newScale.x / aspectRatio;
-            }
-            newScale.y = newy;
+                newx = newScale.y / aspectRatio;
+           }
+
             newScale.x = newx;
+            newScale.y = newy;
         }
-
-        // set the new local scale on both images.
-        Display_Image_1.transform.localScale = newScale;
-        Display_Image_2.transform.localScale = newScale;
-
-        // set the drawable sprite to the first image.
-        Sprite spr = Display_Image_1.GetComponent<SpriteRenderer>().sprite;
-        // Initialize clean pixels to use
-        m_clean_colours_array = new Color[(int)spr.rect.width * (int)spr.rect.height];
-        for (int x = 0; x < m_clean_colours_array.Length; x++)
-            m_clean_colours_array[x] = Reset_Colour;
+        // set the new local scale on display gameobject.
+        m_display_image.transform.localScale = newScale;
 
         Debug.Log("Emulator display size (Image Pixels): x" + sprite_size.x + " y" + sprite_size.y+" (P->LS) x"+local_sprite_size.x+" y"+local_sprite_size.y);
         Debug.Log("Emulator display size (LS): x" + (newScale.x*local_sprite_size.x)+" y"+(newScale.y*local_sprite_size.y));
@@ -87,41 +107,44 @@ public class EmuGraphicsAdapter : MonoBehaviour
     protected void switchBuffers()
     {
         // apply all pixel changes to the draw texture.
-//        clearDrawImage();
         ApplyMarkedPixelChanges();
 
-        // first hide the actual image.
-        // completely disable: m_actual_display_image.SetActive(false); // false to hide, true to show
-        m_actual_display_image.GetComponent<Renderer>().enabled = false;
-        if (m_actual_display_image == Display_Image_1)
-        {
-            // also set the draw image to the opposite image.
-            m_actual_display_image = Display_Image_2;
-            m_actual_draw_image = Display_Image_1;
-        }else{
-            m_actual_display_image = Display_Image_1;
-            m_actual_draw_image = Display_Image_2;
-        }
+        // set the old stuff to the displayed image.
+        m_display_renderer.sprite = m_buffer_sprites[m_actualBufferIndex];
 
-        // set the drawable sprite to the new drawing image.
-        m_drawable_sprite = m_actual_draw_image.GetComponent<SpriteRenderer>().sprite;
-        // set the drawable texture to the new drawing image.
+        // set next buffer image.
+        m_actualBufferIndex++;
+        if (m_actualBufferIndex >= Buffer_Size)
+            m_actualBufferIndex = 0;
+
+        // set the new drawables.
+        m_drawable_sprite = m_buffer_sprites[m_actualBufferIndex];
         m_drawable_texture = m_drawable_sprite.texture;
-        // eventually set the drawable texture pixel array.
+
         m_cur_colors = m_drawable_texture.GetPixels32();
 
-        // show the new image.
-        m_actual_display_image.GetComponent<Renderer>().enabled = true;
+        clearDrawArray();
     }
 
-    // Start is called before the first frame update
-    void Awake()
+    // create new textures for a new display size.
+    public void setEmuScreenSize(int width, int height)
     {
-        Debug.Log("EmuGraphicsAdapter started.");
-        // initialize the buffers.
-        m_actual_display_image = Display_Image_2;
+        m_buffer_sprites = new Sprite[Buffer_Size];
 
-        // resize the displays on startup.
+        // create the buffer sprites.
+        for (int i = 0; i < Buffer_Size; i++)
+        {
+            Debug.Log("Creating buffer image #" + i);
+            Texture2D tex = new Texture2D(width, height);
+            tex.filterMode = FilterMode.Point;
+            //tex.Apply(false);
+            Sprite spr = Sprite.Create(tex, new Rect(0, 0, width, height), new Vector2(0.5f, 0.5f));
+            m_buffer_sprites[i] = spr;
+        }
+
+        m_display_renderer.sprite = m_buffer_sprites[0];
+
+        // resize the displays.
         resized();
 
         // initialize the buffers, set the draw image etc.
@@ -132,12 +155,26 @@ public class EmuGraphicsAdapter : MonoBehaviour
     void setKeepAspectRatio(bool setflag)
     {
         keepAspectRatio = setflag;
+#if (UNITY_EDITOR)
+        m_oldKeepAspectRatio = setflag;
+#endif
         resized();
     }
 
     // Update is called once per frame
     void Update()
     {
+#if (UNITY_EDITOR)
+        // maybe the keep aspect ratio flag has changed.
+        // this will only happen in editor when you click the flag box.
+        // else, setKeepAspectRatio(bool setflag) should be used.
+        if(keepAspectRatio!=m_oldKeepAspectRatio)
+        {
+            m_oldKeepAspectRatio = keepAspectRatio;
+            resized();
+        }
+#endif
+
         MarkPixelToChange(10, 10, Color.green);
         MarkPixelToChange(11, 11, Color.yellow);
         MarkPixelToChange(15, 15, Color.yellow);
@@ -163,19 +200,16 @@ public class EmuGraphicsAdapter : MonoBehaviour
         if (!m_drawable_texture)
             return;
 
-        // first, clean the image with the reset color. all pixel changes have to be made before.
-        // we do not apply the clear colours to the textur right now (see clearDrawImage), because
-        // it's faster if we draw the new image first.
-        m_drawable_texture.SetPixels(m_clean_colours_array);
-
         m_drawable_texture.SetPixels32(m_cur_colors);
         m_drawable_texture.Apply();
     }
 
     // clear the drawing image.
-    public void clearDrawImage()
+    public void clearDrawArray()
     {
-        m_drawable_texture.SetPixels(m_clean_colours_array);
-        m_drawable_texture.Apply();
+        for(int i=0;i<m_cur_colors.Length;i++)
+        {
+            m_cur_colors[i] = Reset_Colour;
+        }
     }
 }
